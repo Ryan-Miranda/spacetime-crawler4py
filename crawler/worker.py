5b8c805a9063e320a9a38279121740cf2f286e8e
Subhamoy Karmakar <subhamoykarmakar224@gmail.com>
2022-02-10 12:07:08 -0800

    Total Word Count, Each Page word count, Save Page

--- crawler/worker.py
+++ crawler/worker.py
@@ -3,6 +3,7 @@ from threading import Thread
 from inspect import getsource
 from utils.download import download
 from utils import get_logger
+from utils.Tokenizer import Tokenizer
 import scraper
 import time
 
@@ -12,6 +13,7 @@ class Worker(Thread):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
+        self.tokenizer = Tokenizer()
         # basic check for requests in scraper
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests from scraper.py"
         super().__init__(daemon=True)
@@ -26,7 +28,7 @@ class Worker(Thread):
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
+            scraped_urls = scraper.scraper(tbd_url, resp, self.tokenizer)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
--- scraper.py
+++ scraper.py
@@ -1,13 +1,14 @@
 import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+from utils import get_urlhash
 
-def scraper(url, resp):
-    links = extract_next_links(url, resp)
+def scraper(url, resp, tokenizer):
+    links = extract_next_links(url, resp, tokenizer)
     return [link for link in links if is_valid(link)]
 
 
-def extract_next_links(url, resp):
+def extract_next_links(url, resp, tokenizer):
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -23,6 +24,9 @@ def extract_next_links(url, resp):
         return []
 
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    url_hash = get_urlhash(url)
+    save_page(url_hash, soup)
+    calculate_page_metric(soup, url_hash, url, tokenizer)
     found_links = soup.find_all('a', href=True)
     links = []
 
@@ -77,3 +81,26 @@ def is_valid(url):
         print ("TypeError for ", parsed)
         # raise
         return False
+
+
+def save_page(url_hash, data):
+    file_path = './tmp/pages/' + str(url_hash)
+    with open(file_path, 'w') as f:
+        f.write(str(data))
+
+
+# wordCount, hashVal, url, top50CommonWords
+def add_page_index(total_count, url_hash, url, word_count_dict):
+    with open('./tmp/pg_index.txt', 'a') as f:
+        f.write(str(total_count) + '\t' + url_hash + '\t' + url + '\t' + word_count_dict + '\n')
+
+
+def calculate_page_metric(soup, url_hash, url, tokenizer):
+    text = soup.get_text(separator="\n", strip=True)
+    ln = 0
+    word_count = tokenizer.word_tokenizer_count(text)
+    word_count = sorted(word_count.items(), key=lambda item: item[1], reverse=True)
+    s = ''
+    for w, cnt in word_count:
+        s = s + w + ',' + str(cnt) + '|'
+    add_page_index(len(word_count), url_hash, url, s)
