8856d4c9c06936c82eaa456779b6594278d52b46
Ryan Miranda <ryanm7082@gmail.com>
2022-02-17 19:54:33 -0800

    added final changes to my script and subhas to generate report

--- scraper.py
+++ scraper.py
@@ -67,9 +67,9 @@ def is_valid(url, oldUrl=None):
 
         parsed = urlparse(url)
 
-        # parsed[:3] = (scheme, domain, path), if any of these are empty string, we want to return because
-        # the URL is badly formed
-        if any(p == "" for p in parsed[:3]):
+        # parsed[:2] = (scheme, domain, path), if any of the first 2 are empty string, we want to return because
+        # the URL is badly formed, but if path is empty thats fine
+        if any(p == "" for p in parsed[:2]):
             return False
 
         if parsed.scheme not in {"http", "https"}:
@@ -104,6 +104,7 @@ def is_valid(url, oldUrl=None):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|java|py|rkt|io|odc|r|m|diff"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())) and (
                    not re.match(r".*/pdf/", parsed.path.lower()))
 
